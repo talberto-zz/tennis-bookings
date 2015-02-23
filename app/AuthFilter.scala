82b3607bff739c647748e939e1d33e1e21730d4a
Tomas Rodriguez <trodriguez@localhost.localdomain>
2015-02-23 21:57:17 +0100

    Finished HTTP basic auth. Closes #1

--- app/AuthFilter.scala
+++ app/AuthFilter.scala
@@ -1,11 +1,23 @@
 import play.api.Logger
+import play.api.Play
+
 import play.api.mvc._
 import play.api.mvc.Results._
+
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 
 import scala.concurrent.Future
 
 class AuthFilter extends Filter {
+  lazy val username: String = Play.current.configuration.getString("http.auth.username").get
+  lazy val password: String = Play.current.configuration.getString("http.auth.password").get
+  
+  val unauthResult = Future {
+    Unauthorized.withHeaders(("WWW-Authenticate", "Basic realm=\"myRealm\""))
+  }
+  
+  val logger = Logger(getClass())
+  
   def decodeBasicAuth(auth: String) = {
     val baStr = auth.replaceFirst("Basic ", "")
     val Array(user, pass) = new String(new sun.misc.BASE64Decoder().decodeBuffer(baStr), "UTF-8").split(":")
@@ -18,8 +30,8 @@ class AuthFilter extends Filter {
       
       (user, pass) match {
         case ("tennis", "tennis") => nextFilter(requestHeader)
-        case _ => Future(Unauthorized)
+        case _ => unauthResult
       }
-    }.getOrElse(Future(Unauthorized))
+    }.getOrElse(unauthResult)
   }
 }
