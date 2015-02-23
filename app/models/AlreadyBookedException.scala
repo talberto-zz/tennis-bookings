f61de1743953482755344ed1b6eebe733fef2b5f
Tomas Rodriguez <rodriguez@progiweb.com>
2015-02-23 13:27:39 +0100

    Fixed exception

--- app/models/AlreadyBookedException.scala
+++ app/models/AlreadyBookedException.scala
@@ -1,6 +1,6 @@
 package models
 
-class AlreadyBookedException(msg: String = "", throwable: Throwable = null) extends RuntimeException {
+class AlreadyBookedException(msg: String = "", throwable: Throwable = null) extends RuntimeException(msg, throwable) {
 
   def this(booking: Booking) {
     this(s"The court [${booking.court}] is already booked at day [${booking.date}] and time [${booking.time}]")
