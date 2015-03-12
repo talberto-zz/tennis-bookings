874d6f3771f09cbe4440ad71cd40ab036332bf65
Tomas Rodriguez <rodriguez@progiweb.com>
2015-03-12 16:16:11 +0100

    Restore accidentaly removed tests

---
+++ test/models/BookingsReminderPageSpec.scala
@@ -0,0 +1,41 @@
+package models
+
+import org.joda.time.DateTime
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+import org.specs2.mutable._
+import org.specs2.runner._
+
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class BookingsReminderPageSpec extends Specification {
+
+  "The 'BookingsReminderPage'" should {
+    "when closed we arrive to CourtsTablePage" in new BookingsReminderPageSpecBeforeAfter {
+      bookingsReminderPage.goToCourtsTable
+      CourtsTablePage.isCurrentPage(driver) must equalTo(true)
+    }
+    
+    "recognize it's page" in new BookingsReminderPageSpecBeforeAfter {
+      BookingsReminderPage.isCurrentPage(driver) must equalTo(true)
+      driver.get("http://www.google.com")
+      BookingsReminderPage.isCurrentPage(driver) must equalTo(false)
+    }
+  }
+}
+
+trait BookingsReminderPageSpecBeforeAfter extends After {
+  val url = "http://localhost:8080/bookings_reminder/"
+  val conf = BookingsReminderPageConf()
+  val driver: WebDriver = new ChromeDriver
+  
+  // Navigate to the page
+  val bookingsReminderPage = BookingsReminderPage(driver, conf)
+  driver.get(url)
+  def after = {
+    driver.close
+  }
+}
---
+++ test/models/CourtsTablePageSpec.scala
@@ -0,0 +1,60 @@
+package models
+
+import org.joda.time.DateTime
+
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+
+import org.specs2.mutable._
+import org.specs2.runner._
+
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
+class CourtsTablePageSpec extends Specification {
+  
+  "The 'CourtsTablePage'" should {
+    "open the 'BookingDetailsPage' when clicked twice over a court" in new CourtsTablePageSpecBeforeAfter {
+      val booking = Booking(dateTime = DateTime.now().withHourOfDay(17), court = 15)
+      courtsTablePage.book(booking)
+      
+      BookingDetailsPage.isCurrentPage(driver) must equalTo(true)
+    }
+    
+    "'canBook' returns false when the court is already booked" in new CourtsTablePageSpecBeforeAfter {
+      val booking = Booking(dateTime = DateTime.now().withHourOfDay(18), court = 12)
+      courtsTablePage.canBook(booking) must equalTo(false)
+    }
+    
+    "'canBook' returns true when the court isn't already booked" in new CourtsTablePageSpecBeforeAfter {
+      val booking = Booking(dateTime = DateTime.now().withHourOfDay(17), court = 15)
+      courtsTablePage.canBook(booking) must equalTo(true)
+    }
+    
+    "recognize it's page" in new CourtsTablePageSpecBeforeAfter {
+      CourtsTablePage.isCurrentPage(driver) must equalTo(true)
+      driver.get("http://www.google.com")
+      CourtsTablePage.isCurrentPage(driver) must equalTo(false)
+    }
+    
+    "throws exception when bookings limit is reached" in new CourtsTablePageSpecBeforeAfter {
+      driver.get("http://localhost:8080/bookings_limit_reached")
+      val booking = Booking(dateTime = DateTime.now().withHourOfDay(20), court = 15)
+      courtsTablePage.book(booking) must throwA[BookingsLimitReachedException]
+    }
+  }
+}
+
+trait CourtsTablePageSpecBeforeAfter extends After {
+  val url = "http://localhost:8080/courts_table/"
+  val conf = CourtsTablePageConf()
+  val driver: WebDriver = new ChromeDriver
+  
+  // Navigate to the page
+  driver.get(url)
+  val courtsTablePage = CourtsTablePage(driver, conf)
+  
+  def after = {
+    courtsTablePage.driver.close
+  }
+}
