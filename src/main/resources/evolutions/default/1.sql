aa698aaf7b6aea9aa928dbabfee3a34910436f56
Tomas Rodriguez <trodriguez@xebia.fr>
2016-02-17 10:20:47 +0100

    Rename bookings to reservations in evolution script

--- src/main/resources/evolutions/default/1.sql
+++ src/main/resources/evolutions/default/1.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-create table "bookings" (
+create table "reservations" (
 	"id" bigserial not null,
 	"creationDate" timestamp with time zone not null,
 	"lastModified" timestamp with time zone not null,
@@ -7,7 +7,7 @@ create table "bookings" (
 	"court" smallint not null,
 	"status" integer not null);
 
-alter table "bookings" add constraint "pk_bookings" primary key ("id");
+alter table "reservations" add constraint "pk_reservations" primary key ("id");
 
 create table "comments" (
 	"id" bigserial not null,
@@ -17,12 +17,12 @@ create table "comments" (
 	"bookingId" bigint not null);
 	
 alter table "comments" add constraint "pk_comments" primary key ("id");
-alter table "comments" add constraint "fk_comments_bookings_id" foreign key ("bookingId") references "bookings"("id") on delete cascade on update cascade;
+alter table "comments" add constraint "fk_comments_reservations_id" foreign key ("bookingId") references "reservations"("id") on delete cascade on update cascade;
 
 # --- !Downs
-alter table "bookings" drop constraint "pk_bookings";
-drop table "bookings";
+alter table "reservations" drop constraint "pk_reservations";
+drop table "reservations";
 
 alter table "comments" drop constraint "pk_comments";
-alter table "comments" drop constraint "fk_comments_bookings_id";
+alter table "comments" drop constraint "fk_comments_reservations_id";
 drop table "comments";
